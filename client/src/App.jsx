2bf186372c77a76a749cdbd248175f086cba4e1d
silma subah <silmasubah105@gmail.com>
2026-08-16 16:46:24 +0600

    msg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route,Routes } from "react-router";
+import Home from './pages/Home';
+import ProtectedRoute from './layouts/ProtectedRoute';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import RootLayOut from './layouts/RootLayOut';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+     <Routes>
+       <Route path='/' element={<RootLayOut/>}>
+       <Route index element={<Home/>}/>
+       <Route path='/auth' element={<ProtectedRoute/>}>
+         <Route path='login' element={<Login/>} />
+         <Route path='register' element={<Register/>} />
+       </Route>
+       </Route>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
